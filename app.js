5c385cf913c3c476ebda35b5a27ded6f975fa1eb
smackeem <smackeem@outlook.com>
2023-10-23 12:00:31 -0400

    Changed the code to something simpler before editiing

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-// JavaScript for interactivity and animation
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('button');
-    const letter = document.getElementById('letter');
-    const backButton = document.getElementById('back-button');
-    const confettiContainer = document.getElementById('confetti-container');
+const openLetterButton = document.getElementById('openLetter');
+const letter = document.getElementById('letter');
 
-    button.addEventListener('click', function() {
-        // Hide the opening page and show the letter
-        confettiContainer.style.display = 'none';
-        letter.style.display = 'block';
-    });
-
-    backButton.addEventListener('click', function() {
-        // Show the opening page and hide the letter
-        confettiContainer.style.display = 'block';
-        letter.style.display = 'none';
-    });
-
-    // Create animated confetti
-    const confettiColors = ['#f00', '#0f0', '#00f', '#ff0', '#f0f', '#0ff'];
-    for (let i = 0; i < 100; i++) {
-        const confetti = document.createElement('div');
-        confetti.style.backgroundColor = confettiColors[Math.floor(Math.random() * confettiColors.length)];
-        confetti.style.left = Math.random() * 100 + 'vw';
-        confetti.style.animationDuration = Math.random() * 2 + 1 + 's';
-        confettiContainer.appendChild(confetti);
-    }
+openLetterButton.addEventListener('click', () => {
+    letter.style.display = 'block';
+    openLetterButton.style.display = 'none';
 });
